d16376bfb19f24e3cc9fd35f811a450459963942
botelja <ivo.surjak@gmail.com>
2019-10-25 20:58:28 +0200

    Get data from github api

--- src/App.js
+++ src/App.js
@@ -2,14 +2,29 @@ import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import Users from './components/Users';
+import axios from 'axios';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: [],
+      loading: false
+    };
+  }
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get('https://api.github.com/users');
+
+    this.setState({ users: res.data, loading: false });
+  }
   render() {
+    const { users } = this.state;
     return (
       <div className="App">
         <Navbar />
         <div className="container mt-4">
-          <Users />
+          <Users users={users} />
         </div>
       </div>
     );
