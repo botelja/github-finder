b7441ba716f674478b1f68dbc7ee7e951d7bf339
botelja <ivo.surjak@gmail.com>
2019-10-25 20:40:03 +0200

    Refactor class component to functional component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class Navbar extends Component {
-  render() {
-    const { title, icon } = this.props;
-    return (
-      <nav className="navbar navbar-dark bg-dark">
-        <a className="navbar-brand" href="!#">
-          <i className={icon}></i> {title}
-        </a>
-      </nav>
-    );
-  }
-}
+const Navbar = ({ title, icon }) => {
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <a className="navbar-brand" href="!#">
+        <i className={icon}></i> {title}
+      </a>
+    </nav>
+  );
+};
 
 Navbar.defaultProps = {
   title: 'Github Finder',
--- src/components/UserItem.jsx
+++ src/components/UserItem.jsx
@@ -1,25 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class UserItem extends Component {
-  render() {
-    const { login, avatar_url, html_url } = this.props.user;
-    return (
-      <div className="card text-center">
-        <img
-          src={avatar_url}
-          alt=""
-          className="my-3  mx-auto d-block rounded-circle"
-          style={{ width: '60px' }}
-        />
-        <h3>{login}</h3>
-        <div>
-          <a href={html_url} className="btn btn-primary btn-sm mb-2">
-            More
-          </a>
-        </div>
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        alt=""
+        className="my-3  mx-auto d-block rounded-circle"
+        style={{ width: '60px' }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <a href={html_url} className="btn btn-primary btn-sm mb-2">
+          More
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired
+};
 
 export default UserItem;
